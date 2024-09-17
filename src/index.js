02abdb81acc8f09dbfc21f6ad00fe763dadeb451
Ramon Reyes <ramonrc@users.noreply.github.com>
2024-09-17 17:43:34 -0600

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,18 @@
+
+
+var Home = {
+    view: function() {
+        return(m("main",
+        m("h1", {class: "title"}, "Sistemas"),
+        m('nav', m('ul', {class: "menu menu--background"},
+            m('li', {class: "menu__button  menu__button--arrow"}, "Productos",
+                m('div', {class: "dropdown-container"}, m('ul',
+                    m('li', {class: "menu__button"}, m("a", {href: "#!/ch"}, "Chocolate")),
+                    m('li', {class: "menu__button"}, m("a", {href: "#!/ca"}, "Café"))
+                    )))
+            )),
+        m("div", {class: "title"}, m('h1', "San Francisco, Soteapan"))
+        ))
+    }
+}
+
